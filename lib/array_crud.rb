31d16871a7de684672a62ec12edc9fe82ea15077
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-02-26 17:19:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_food = "taco"
-  food << new_food
+  
+  food << element
 end
 
 def add_element_to_start_of_array(array, element)
